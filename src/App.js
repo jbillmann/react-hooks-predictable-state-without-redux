d976e0f31876e6eff3788107d385dd1898b77107
Jeremiah Billmann <jcbillmann@yahoo.com>
2018-12-10 07:16:33 -0700

    Remove ViewWrapper

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import ViewWrapper from './ViewWrapper';
+import { StoreProvider } from "./providers/Store";
+import Name from './views/Name';
 import logo from './logo.svg';
 import './App.css';
 
 export default function App(props) {
   return (
+    <StoreProvider>
       <div className="App">
         <header className="App-header">
           <h2>Predictable state example using React Hooks and Context API without Redux</h2>
           <img src={logo} className="App-logo" alt="logo" />
-          <ViewWrapper />
+          <Name />
         </header>
       </div>
+    </StoreProvider>
   );
 }
